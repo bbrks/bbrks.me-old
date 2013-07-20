e90e0f12351030afee4b76174ee62b306e0d5faf
Ben Brooks <ben@bbrks.me>
2013-07-20 19:53:35 +0100

    Updated config and added Reading Time plugin

---
+++ _plugins/reading_time.rb
@@ -0,0 +1,17 @@
+# Outputs the reading time
+
+# Read this in “about 4 minutes”
+# Put into your _plugins dir in your Jekyll site
+# Usage: Read this in about {{ page.content | reading_time }}
+
+module ReadingTimeFilter
+	def reading_time( input )
+		words_per_minute = 180
+
+		words = input.split.size;
+		minutes = ( words / words_per_minute ).floor
+		minutes > 0 ? " ~ #{minutes} minuted read" : ""
+	end
+end
+
+Liquid::Template.register_filter(ReadingTimeFilter)
